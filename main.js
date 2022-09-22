b17006c7f4160144cf000d95967c503bff208937
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-22 16:02:46 -0500

    Finalizado

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const menu = document.querySelector('_bars')
+const menu = document.querySelector('.bars');
+let menuState = false;
+menu.addEventListener('click', showMenu);
+
+function showMenu()
+{
+    if (!menuState)
+    {
+        menu.classList.add("is-active");
+        document.querySelector(".nav__links").classList.add("open");
+        menuState = true;
+    }
+    else{
+        menu.classList.remove("is-active");
+        document.querySelector(".nav__links").classList.remove("open");
+        menuState = false;
+    }
+}
