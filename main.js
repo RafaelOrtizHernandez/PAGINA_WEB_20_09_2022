2e29d045bb3430702fb8b0c043d33cfa7f2122ca
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-23 23:02:31 -0500

    Selectores

--- main.js
+++ main.js
@@ -16,3 +16,16 @@ function showMenu()
         menuState = false;
     }
 }
+
+document.querySelector(".titulo").insertAdjacentText("beforeend", "Coderider");
+document.querySelector(".home").insertAdjacentText("beforeend", "Inicio");
+document.querySelector(".file-alt").insertAdjacentText("beforeend", "Proyectos");
+document.querySelector(".folder-open").insertAdjacentText("beforeend", "Grupos");
+document.querySelector(".comment-dots").insertAdjacentText("beforeend", "Dialogos");
+document.querySelector(".map-marked-alt").insertAdjacentText("beforeend", "Contacto");
+
+console.log(document.querySelectorAll(".logo"));
+
+    
+
+
